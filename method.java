1447f81cf3532cf428a1d4c49653ca12d355a937
Ibadullah Saif <146116526+Saif544346@users.noreply.github.com>
2023-12-10 15:06:05 +0600

    Add files via upload

---
+++ method.java
@@ -0,0 +1,19 @@
+
+package com.mycompany.oopjava;
+
+public class method {
+    String name,gender;
+    int id;
+    void setinformation(String x,String y,int z){
+        name=x;
+        gender=y;
+        id=z;
+        
+    }
+    void dispaly(){
+        System.out.println("Name:"+name);
+        System.out.println("Id:"+id);
+        System.out.println("Gender:"+gender);
+    }
+    
+}
---
+++ test2.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.oopjava;
+public class test2 {
+    public static void main(String[] args) {
+        method ob1=new method();
+       ob1.setinformation("saif", "male", 101);
+       ob1.dispaly();
+       method ob2=new method();
+       ob2.setinformation("x", "male", 102);
+       ob2.dispaly();
+    }
+    
+}
