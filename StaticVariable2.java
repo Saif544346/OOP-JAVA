622bcf46bdcc55aea5a70c635c10a0ddd52b427c
Ibadullah Saif <146116526+Saif544346@users.noreply.github.com>
2023-12-12 02:29:15 +0600

    Add files via upload

---
+++ StaticVariable2.java
@@ -0,0 +1,9 @@
+
+package com.mycompany.oopjava;
+public class StaticVariable2 {
+   static String University="Green University of Bangladesh" ;
+   StaticVariable2(String s){
+       University=s;
+   }
+    
+}
---
+++ test7.java
@@ -0,0 +1,8 @@
+
+package com.mycompany.oopjava;
+public class test7 {
+    public static void main(String[] args) {
+        System.out.println("unversity name :"+StaticVariable2.University);
+    }
+    
+}
