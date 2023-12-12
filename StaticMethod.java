ea3485565f99114ea5074c59fd85cf476139f230
Ibadullah Saif <146116526+Saif544346@users.noreply.github.com>
2023-12-12 15:33:03 +0600

    Add files via upload

---
+++ StaticMethod.java
@@ -0,0 +1,11 @@
+
+package com.mycompany.oopjava;
+public class StaticMethod {
+    void dis1(){
+        System.out.println("i am from dis1");
+    }
+   static void dis2(){
+        System.out.println("i am from dis2");
+    }
+    
+}
---
+++ test9.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.oopjava;
+public class test9 {
+    public static void main(String[] args) {
+        StaticMethod ob1=new StaticMethod();
+        ob1.dis1();
+        StaticMethod.dis2();
+    }
+    
+}
