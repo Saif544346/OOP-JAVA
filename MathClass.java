be244067eec5e360c10b46172f8351bd6eda9d86
Ibadullah Saif <146116526+Saif544346@users.noreply.github.com>
2023-12-12 21:58:52 +0600

    Add files via upload

---
+++ MathClass.java
@@ -0,0 +1,19 @@
+
+package com.mycompany.oopjava;
+public class MathClass {
+    public static void main(String[] args) {
+        System.out.println(""+Math.abs(-12));
+        System.out.println(""+Math.sqrt(12));
+        System.out.println(""+Math.PI);
+        System.out.println(""+Math.pow(2, 3));
+        System.out.println(""+Math.log(1));
+        System.out.println(""+Math.max(-1.34,-2.54));
+        System.out.println(""+Math.min(-1.65,-5.65));
+        System.out.println(""+Math.ceil(-5.65));
+        System.out.println(""+Math.floor(-5.20));
+        
+        
+        
+    }
+    
+}
