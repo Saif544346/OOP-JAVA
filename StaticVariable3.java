2b913324e95a6d85b879ac70e88fb212e35f607a
Ibadullah Saif <146116526+Saif544346@users.noreply.github.com>
2023-12-12 15:22:03 +0600

    Add files via upload

---
+++ StaticVariable3.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.oopjava;
+
+public class StaticVariable3 {
+   static int count=0;
+    StaticVariable3(){
+        count++;
+    }
+    void display(){
+        System.out.println("value :"+count);
+    }
+    
+}
---
+++ test8.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.oopjava;
+public class test8 {
+    public static void main(String[] args) {
+        StaticVariable3 ob1=new StaticVariable3();
+        ob1.display();
+        StaticVariable3 ob2=new StaticVariable3();
+        ob2.display();
+        StaticVariable3 ob3=new StaticVariable3();
+        ob3.display();
+    }
+    
+}
